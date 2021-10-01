6a6f637be65d471d819dc789fa8a254013b17f31
chrismwalker-portfolio <73894394+chrismwalker-portfolio@users.noreply.github.com>
2021-10-01 17:28:15 +0100

    Move to new provider for country flag image

--- resources/js/script.js
+++ resources/js/script.js
@@ -331,13 +331,11 @@ $(document).ready(function() {
         // Remove any image elements from the last time the modal was shown
         $('#modal-card-country-flag').empty();
 
-        if (countryInfo.flag) {
-            let countryFlag = $(document.createElement('img'));
-            countryFlag.attr('src', countryInfo.flag);
-            countryFlag.attr('alt', `Flag of ${countryInfo.name}`);
-            countryFlag.attr('aria-label', `Flag of ${countryInfo.name}`);
-            countryFlag.appendTo('#modal-card-country-flag');
-        }
+        let countryFlag = $(document.createElement('img'));
+        countryFlag.attr('src', `https://flagcdn.com/${countryInfo.alpha2Code.toLowerCase()}.svg`);
+        countryFlag.attr('alt', `Flag of ${countryInfo.name}`);
+        countryFlag.attr('aria-label', `Flag of ${countryInfo.name}`);
+        countryFlag.appendTo('#modal-card-country-flag');
 
         const countryName = countryInfo.name ? countryInfo.name : errorText;
         const countryRegion = countryInfo.subregion ? countryInfo.subregion : errorText;
